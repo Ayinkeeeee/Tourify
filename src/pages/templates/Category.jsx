82cb1d56e8fa10b98b699b54ff71817df01edeb9
tomisin <tomjames156@gmail.com>
2024-03-23 10:34:03 +0100

    added categories buttons

--- src/pages/templates/Category.jsx
+++ src/pages/templates/Category.jsx
@@ -1,18 +1,33 @@
 function Category({category}) {
+  const imagePath = `assets/images`
+
   return (
-    <>
+    <div sty className="category" id={`${category.categoryName}`}>
         <section className="start">
-            <img src={require("../../assets/images/food.png")} alt="food" />
-            <h1>{category.name}</h1>
+            <img src={require(`../../${imagePath}/${category.categoryImage}.png`)} alt="food" />
+            <h1>{category.headingText}</h1>
         </section>
-    </>
+        <main>
+          <p>{category.introText}</p>
+          <div className="categoriesBtns">
+            {category.categories && category.categories.map((category, index) => (
+              <button key={index} className={`categoryBtn ${index % 2 === 0 ? 'blue' : 'black'}`}>
+                {category}
+              </button>
+            ))}
+          </div>
+        </main>
+    </div>
   )
 }
 
 Category.defaultProps = {
     category: {
-        name: "Food & Drinks in Abuja",
-        categoryImage: "food.png"
+      categoryName: 'food',
+      headingText: "Food & Drinks in Abuja",
+      categoryImage: "food",
+      introText: "Of course you cant come to Abuja without trying our best spots, where every bite tells a story and every sip is an experience to remember. Indulge your senses as we take you on a journey through our vibrant world of food and drinks, showcasing the best flavors our region has to offer.",
+      categories: ["Fine Dining", "Fast Food", "Native Food", "Ice-Cream", "Chinese / Asian", "Italian", "Shawarma", "Cafe"]
     }
 }
 
