7062c57f96fa063b55d3d7d0f4709c25a0d74268
Abubakar Kamarudeen <abukamar1805@gmail.com>
2024-04-02 07:42:55 -0700

    Updated tour guide page

--- src/components/items/TourGuide.jsx
+++ src/components/items/TourGuide.jsx
@@ -2,7 +2,7 @@ import tomski from "../../assets/images/tomski.png"
 import quote from "../../assets/icons/quote.svg"
 import { motion } from "framer-motion";
 
-function TourGuide() {
+function TourGuide({data}) {
   return (
     <motion.div 
       className="tourGuideItem"
@@ -10,15 +10,27 @@ function TourGuide() {
       whileInView={{opacity: 1, y: 0, transition: {duration: 0.45}}}
       viewport={{once: true, amount: 0.5 }}
     >
-        <img className="person" src={tomski} alt="tomi" />
-        <p className="fname">Tomi</p>
-        <p className="specialty">Domestic tours</p>
-        <p className="statement">I'm Chinaza, your friendly neighbourhood tour guide with a passion for showcasing the rich tapestry of culture history, and natural beauty that our destinations have to offer
+        <img className="person" src={require(`../../assets/images/${data.person}`)} alt="tomi" />
+        <p className="fname">{data.fname}</p>
+        <p className="specialty">{data.specialty}</p>
+        <p className="statement">
+          {data.statement}
         <img className="q1" src={quote} alt="quote" />
-        <img className="q2" src={quote} alt="quote" /></p>
-        <p className="phone">07068833047</p>
+        <img className="q2" src={quote} alt="quote" />
+        </p>
+        <p className="phone">{data.phone}</p>
     </motion.div>
   )
+}
+
+TourGuide.defaultProps = {
+  data: {
+    person: "tomski.png",
+    fname: 'Tomi',
+    specialty: 'Domestic tours',
+    statement: "I'm Chinaza, your friendly neighbourhood tour guide with a passion for showcasing the rich tapestry of culture history, and natural beauty that our destinations have to offer",
+    phone: "07068833047"
+  }
 }
 
 export default TourGuide
--- src/pages/TourGuides.jsx
+++ src/pages/TourGuides.jsx
@@ -23,25 +23,104 @@ function TourGuides() {
             <section className="tour_guides">
                 <h1>Our Tour Guides</h1>
                 <div className="guides">
-                    <TourGuide/>
-                    <TourGuide/>
-                    <TourGuide/>
-                    <TourGuide/>
-                    <TourGuide/>
-                    <TourGuide/>
-                    <TourGuide/>
-                    <TourGuide/>
-                    <TourGuide/>
-                    <TourGuide/>
-                    <TourGuide/>
+                    <TourGuide data={{
+                            person: "tomski.png",
+                            fname: 'Chinaza',
+                            specialty: 'Domestic tours',
+                            statement: "I'm Chinaza, your friendly neighbourhood tour guide with a passion for showcasing the rich tapestry of culture history, and natural beauty that our destinations have to offer",
+                            phone: "08129606423"
+                        }}/>
+                    <TourGuide data={{
+                            person: "tomski.png",
+                            fname: 'Maryam',
+                            specialty: 'Domestic tours',
+                            statement: "Hey! I'm Maryam, your friendly and knowledgeable tour guide with a deep passion for showcasing the vibrant culture and natural beauty of our beloved destinations. ",
+                            phone: "08095985405"
+                        }}/>
+                    <TourGuide data={{
+                            person: "tomski.png",
+                            fname: 'Gabriel',
+                            specialty: 'Domestic tours',
+                            statement: "I'm Gabriel, your friendly neighbourhood tour guide with a passion for showcasing the rich tapestry of culture history, and natural beauty that our destinations have to offer",
+                            phone: "09094784251"
+                        }}/>
+                    <TourGuide data={{
+                            person: "tomski.png",
+                            fname: 'Kamar',
+                            specialty: 'Domestic tours',
+                            statement: "I'm Kamar, your friendly neighbourhood tour guide with a passion for showcasing the rich tapestry of culture history, and natural beauty that our destinations have to offer",
+                            phone: "08143104084"
+                        }}/>
+                    <TourGuide data={{
+                            person: "tomski.png",
+                            fname: 'Tomisin',
+                            specialty: 'Domestic tours',
+                            statement: "I'm Tomisin, your friendly neighbourhood tour guide with a passion for showcasing the rich tapestry of culture history, and natural beauty that our destinations have to offer",
+                            phone: "07068293037"
+                        }}/>
+                    <TourGuide data={{
+                            person: "tomski.png",
+                            fname: 'Prince',
+                            specialty: 'Domestic tours',
+                            statement: "I'm Prince, your friendly neighbourhood tour guide with a passion for showcasing the rich tapestry of culture history, and natural beauty that our destinations have to offer",
+                            phone: "09157662986"
+                        }}/>
+                    <TourGuide data={{
+                            person: "tomski.png",
+                            fname: 'Japhet',
+                            specialty: 'Domestic tours',
+                            statement: "I'm Japhet, your friendly neighbourhood tour guide with a passion for showcasing the rich tapestry of culture history, and natural beauty that our destinations have to offer",
+                            phone: "08068852166"
+                        }}/>
+                   <TourGuide data={{
+                            person: "tomski.png",
+                            fname: 'Fadila',
+                            specialty: 'Domestic tours',
+                            statement: "I'm Fadila, your friendly neighbourhood tour guide with a passion for showcasing the rich tapestry of culture history, and natural beauty that our destinations have to offer",
+                            phone: "09135315332"
+                        }}/>
+                    <TourGuide data={{
+                            person: "tomski.png",
+                            fname: 'Waweru',
+                            specialty: 'Domestic tours',
+                            statement: "I'm Waweru, your friendly neighbourhood tour guide with a passion for showcasing the rich tapestry of culture history, and natural beauty that our destinations have to offer",
+                            phone: "08121723376"
+                        }}/>
+                    <TourGuide data={{
+                            person: "tomski.png",
+                            fname: 'Ruden',
+                            specialty: 'Domestic tours',
+                            statement: "I'm Ruden, your friendly neighbourhood tour guide with a passion for showcasing the rich tapestry of culture history, and natural beauty that our destinations have to offer",
+                            phone: "07064191837"
+                        }}/>
+                   <TourGuide data={{
+                            person: "tomski.png",
+                            fname: 'Micheal',
+                            specialty: 'Domestic tours',
+                            statement: "I'm Micheal, your friendly neighbourhood tour guide with a passion for showcasing the rich tapestry of culture history, and natural beauty that our destinations have to offer",
+                            phone: "09018800387"
+                        }}/>
                 </div>
             </section>
             <section className="qna">
                 <h1>Why get a tour guide?</h1>
                 <div className="questions">
-                    <GuideQnA/>
-                    <GuideQnA/>
-                    <GuideQnA/>
+                    <GuideQnA guideQnA={{
+                            question: "Personalized experience!",
+                            answer: "One of the greatest advantages of hiring a tour guide is the ability to tailor your experience to suit your interests, preferences, and pace."
+                        }}
+                        />
+                    <GuideQnA guideQnA={{
+                            question: "Insider Knowledge and Expertise",
+                            answer: "Tour guides are more than just local experts – they're passionate storytellers with an intimate understanding of the destinations they showcase."
+                        }}
+                        />
+                    <GuideQnA guideQnA={{
+                            question: "Access to Exclusive Opportunities",
+                            answer: "Tour guides often have access to exclusive attractions, behind-the-scenes experiences, and off-the-beaten-path destinations that aren't accessible to the general public."
+                        }}
+                        />
+                    
                 </div>
             </section>
             <section className="how-to">
