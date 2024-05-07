595e986e323a7a50a2904e332a0ecba206a4c174
tomisin <tomjames156@gmail.com>
2024-05-07 11:22:53 +0100

    added forgot password page

---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,16 @@
+import eyeSlash from '../assets/icons/visibility_off.svg'
+import eye from '../assets/icons/eye.svg'
+import Header from '../components/sections/Header'
+import Footer from '../components/sections/Footer'
+import { motion } from 'framer-motion'
+import { toast } from 'react-toastify'
+import { useState } from "react"
+import { Link } from "react-router-dom";
+
+function ForgotPassword() {
+  return (
+    <div>ForgotPassword</div>
+  )
+}
+
+export default ForgotPassword
