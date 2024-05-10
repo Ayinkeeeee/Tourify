2d2aad2a417384a2e73ff521fe332b426c6f552b
tomisin <tomjames156@gmail.com>
2024-05-10 09:13:52 +0100

    added profile update functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,21 +2,69 @@ import tomski from "../assets/images/tomski.png"
 import backBtn from "../assets/icons/back.svg"
 import Header from "../components/sections/Header"
 import Footer from "../components/sections/Footer"
+import { doc, getDoc, updateDoc } from "firebase/firestore"
+import { getAuth, updateProfile } from "firebase/auth"
+import { db } from "../firebase.config"
 import { motion } from "framer-motion"
 import { toast } from "react-toastify"
 import { useNavigate } from "react-router-dom"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function Profile() {
+    
     const navigator = useNavigate()
     const [ edit, setEdit ] = useState(false)
     const [ form, setForm ] = useState({
         fname: 'John',
         lname: 'Doe',
-        location: "Abuja, Nigeria",
+        location: "",
         dOB: new Date('2004-03-04'),
         email: 'tourifyuser@mail.com'
     })
+
+    const { fname, lname, location, dOB, email } = form
+
+    const auth = getAuth()
+
+    const getProfile = async () => {
+
+        try{
+
+            const docRef = doc(db, "users", auth.currentUser.uid)
+            const docSnap = await getDoc(docRef)
+
+            if(docSnap.exists()){
+                return docSnap.data()
+            }else{
+                console.log("Document does not exist")
+            }
+        }catch(err){
+            toast.error("Credential Retrieval Failed")
+        }
+    }
+
+    useEffect(() => {
+        if(auth){
+            const profile = getProfile()
+
+            const { displayName } = auth.currentUser
+            const { email, dOB, timeStamp } = profile;
+            const [ fname, lname ] = displayName.split(" ");
+
+            setForm((formData) => (
+                {   
+                    ...formData, 
+                    fname,
+                    lname,
+                    email,
+                    dOB: dOB,
+                    location: profile?.location || '',
+                    dateJoined: timeStamp
+                }
+            ))
+        }
+        
+    }, [])
 
     function formatDateToString(date) {
         const options = { day: 'numeric', month: 'long', year: 'numeric' };
@@ -53,14 +101,31 @@ function Profile() {
         })
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        setEdit(false)
-        console.log(form)
-        toast.success('Profile updated')
+        try{
+
+            const profileRef = doc(db, "users", auth.currentUser.uid);
+            
+            // update location
+            await updateDoc(profileRef, {
+                location: location
+            })
+
+            await updateProfile(auth.currentUser, {
+                displayName: fname + " " + lname
+            })
+
+            setEdit(false)
+            toast.success('Profile Updated')
+        }catch(err){
+            toast.success("Profile Update Failed")
+            return
+        }
+        
     }
 
-  return (
+  return auth.currentUser ? 
     <motion.div
         id="profile"
         initial={{width: 0}}
@@ -80,28 +145,32 @@ function Profile() {
                 <div className="inputItem">
                     <label htmlFor="fname">First Name:</label>
                     { edit ? 
-                    <input value={form.fname} type="text" name="fname" id="fname" onChange={handleChange} /> : 
+                    <input value={fname || ''} type="text" name="fname" id="fname" onChange={handleChange} /> : 
                     <p>{form.fname}</p>}
                 </div>
                 <div className="inputItem">
                     <label htmlFor="lname">Last Name:</label>
                     { edit ? 
-                    <input value={form.lname} type="text" name="lname" id="lname" onChange={handleChange} /> : 
+                    <input value={lname || ''} type="text" name="lname" id="lname" onChange={handleChange} /> : 
                     <p>{form.lname}</p>}
                 </div>
                 <div className="inputItem">
                     <label htmlFor="location">Location:</label>
                     { edit ? 
-                    <input value={form.location} type="text" name="location" id="location" onChange={handleChange} /> : 
+                    <input value={location || ''} type="text" name="location" id="location" onChange={handleChange} /> : 
                     <p>{form.location}</p>}
                 </div>
                 <div className="inputItem">
-                    <label htmlFor="dOB">Date of Birth:</label>
-                    <p>{formatDateToString(form.dOB)}</p>
+                    <label htmlFor="email">Email:</label>
+                    <p>{email}</p>
                 </div>
                 <div className="inputItem">
-                    <label htmlFor="email">Email:</label>
-                    <p>{form.email}</p>
+                    <label htmlFor="dOB">Date of Birth:</label>
+                    <p>{formatDateToString(dOB)}</p>
+                </div>
+                <div className="inputItem">
+                    <label htmlFor="dOJ">Date Joined:</label>
+                    <p>{formatDateToString(form?.dateJoined)}</p>
                 </div>
                 <div className="buttons">
                     {edit ? 
@@ -114,8 +183,9 @@ function Profile() {
             </form>
         </main>
         <Footer/>
-    </motion.div>
-  )
+    </motion.div> : <h1>Not Logged In</h1>
+    
+  
 }
 
 export default Profile
